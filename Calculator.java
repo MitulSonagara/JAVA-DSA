13a120cf11e9599871164b195a17acaec39d456d
Mitul Sonagara <mitulkumarsongara@gmail.com>
2023-11-17 11:34:19 +0530

    claculator

---
+++ Calculator.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.err.println("Enter the operator or press x for close : ");
+            char op = sc.next().trim().charAt(0);
+            if (op == '+' || op == '-' || op == '*' || op == '/' || op == '%') {
+                System.out.print("Enter first number : ");
+                int a = sc.nextInt();
+                System.out.print("Enter second number : ");
+                int b = sc.nextInt();
+                if (op == '+') {
+                    System.out.println("The sum is : " + (a + b));
+                } else if (op == '-') {
+                    System.out.println("The subtraction is : " + (a - b));
+                } else if (op == '*') {
+                    System.out.println("The multiplication is : " + (a * b));
+                } else if (op == '/') {
+                    System.out.println("The division is : " + (a / b));
+                } else if (op == '%') {
+                    System.out.println("The remainder is : " + (a % b));
+                }
+            } else if (op == 'x' || op == 'X') {
+                break;
+            } else {
+                System.out.println("Please enter valid operator!!!");
+            }
+        }
+        sc.close();
+    }
+}
