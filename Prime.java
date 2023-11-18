b88241ea741a9933dedbc979e0d8545f4a891ed9
Mitul Sonagara <mitulkumarsongara@gmail.com>
2023-11-18 11:07:47 +0530

    Prime Number

---
+++ Prime.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Prime {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        n = sc.nextInt();
+        isPrime(n);
+        sc.close();
+    }
+
+
+    static void isPrime(int n) {
+        if (n <= 1) {
+            System.out.println("neither prime nor not prime");
+            return;
+        } else {
+            int count = 2;
+            while (count * count <= n) {
+                if (n % count == 0) {
+                    System.out.println("not prime");
+                    return;
+                }
+                count++;
+            }
+        }
+        System.out.println("Prime");
+        return;
+    }
+}
