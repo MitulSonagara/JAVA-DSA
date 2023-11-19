4b8c7c9eee8b392bd4a2227111c1c215182a5885
Mitul Sonagara <mitulkumarsongara@gmail.com>
2023-11-19 12:25:58 +0530

    reverse array

---
+++ ReverseArr.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class ReverseArr {
+    public static void main(String[] args) {
+        int[] arr1 = { 1, 2, 3, 4, 5, 6 };// even numbers of elements
+        int[] arr2 = { 1, 2, 3, 4, 5 };// odd numbers of elements
+
+        reverse(arr1);
+        reverse(arr2);
+        System.out.println(Arrays.toString(arr1));
+        System.out.println(Arrays.toString(arr2));
+    }
+
+    static void swap(int[] arr, int index1, int index2) {
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+    }
+
+    static void reverse(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start < end) {
+            swap(arr, start, end);
+            start++;
+            end--;
+        }
+    }
+}
