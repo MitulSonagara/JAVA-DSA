78f0b385c98e88fc980b6479a53b2b23f25ff853
Mitul Sonagara <95460188+MitulSonagara@users.noreply.github.com>
2024-02-04 23:40:16 +0530

    Update Maximum.java

--- Maximum.java
+++ Maximum.java
@@ -7,7 +7,7 @@ public class Maximum {
 
     // imagin that array is not empty
     static int max(int[] arr) {
-        int max = arr[0];
+        int max = arr[0]; 
         for (int i = 1; i < arr.length; i++) {
             if (arr[i] > max) {
                 max = arr[i];
